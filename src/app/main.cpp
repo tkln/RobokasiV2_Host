f895ffe3af0f669d328ba0da820877c17c231d13
Miika Lehtimäki <miika.lehtimaki@gmail.com>
2018-10-13 22:19:02 +0300

    main: Add PUMA 560 kinematic chain

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,19 +3,32 @@
 //
 
 #include "SDLApp.hpp"
-#include <kinematics/Hello.hpp>
+#include <kinematics/Chain.hpp>
 #include <gui/Hello.hpp>
 #include <hwio/Hello.hpp>
+#include <iostream>
 
 
 int main(int argv, char** args) {
 
-    kin::Hello kinHello;
-    kinHello.hello();
     gui::Hello guiHello;
     guiHello.hello();
     hwio::Hello hwioHello;
     hwioHello.hello();
+
+    kin::Chain chain;
+    chain.addJoint(kin::Joint(kin::DHMatrix(
+    0.0f, 0.0f, 0.0f, PI*0.5f)));
+    chain.addJoint(kin::Joint(kin::DHMatrix(
+    0.0f, 0.0f, 0.4318f, 0.0f)));
+    chain.addJoint(kin::Joint(kin::DHMatrix(
+    0.15f, 0.0f, 0.0203f, -PI*0.5f)));
+    chain.addJoint(kin::Joint(kin::DHMatrix(
+    0.4318f, 0.0f, 0.0f, PI*0.5f)));
+    chain.addJoint(kin::Joint(kin::DHMatrix(
+    0.0f, 0.0f, 0.0f, -PI*0.5f)));
+    chain.addJoint(kin::Joint(kin::DHMatrix(
+    0.0f, 0.0f, 0.0f, 0.0f)));
 
     SDLApp app(SDLApp::Settings("Robokasi"));
     app.loop();

d31924a64ba07bb1adc51345653d7a99ee90a238
Miika Lehtimäki <miika.lehtimaki@gmail.com>
2018-10-14 00:13:28 +0300

    GUI: add Renderable class

---
+++ src/gui/Renderable.hpp
@@ -0,0 +1,15 @@
+//
+// Created by lehdari on 14.10.2018.
+//
+
+#ifndef ROBOKASIV2_HOST_GUI_RENDERABLE_HPP
+#define ROBOKASIV2_HOST_GUI_RENDERABLE_HPP
+
+
+class Renderable {
+public:
+    virtual void render() = 0;
+};
+
+
+#endif //ROBOKASIV2_HOST_GUI_RENDERABLE_HPP

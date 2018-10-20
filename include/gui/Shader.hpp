a59fb0a1f2c690cec8eee345529623e89f7a56e0
Miika Lehtimäki <miika.lehtimaki@gmail.com>
2018-10-20 19:53:30 +0300

    gui: add Shader class

---
+++ include/gui/Shader.hpp
@@ -0,0 +1,31 @@
+//
+// Created by lehdari on 20.10.2018.
+//
+
+#ifndef ROBOKASIV2_HOST_SHADER_HPP
+#define ROBOKASIV2_HOST_SHADER_HPP
+
+
+#include "GLTypes.hpp"
+
+
+namespace gui {
+
+    class Shader {
+    public:
+        Shader(const std::string& vsFileName, const std::string& fsFileName);
+        ~Shader();
+
+        void useShader(const Mat4GLf& mvp,
+                       const Vec3GLf& color = Vec3GLf(1.0f, 1.0f, 1.0f)) const;
+
+    private:
+        GLuint programId_;
+        GLuint uniformPosition_MVP_;
+        GLuint uniformPosition_Color_;
+    };
+
+} // namespace gui
+
+
+#endif //ROBOKASIV2_HOST_SHADER_HPP

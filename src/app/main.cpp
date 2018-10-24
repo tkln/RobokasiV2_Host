83c93401346b346cc7c2b92417058d0d976536be
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2018-10-24 15:51:15 +0300

    Remove gui::Hello

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,15 +4,12 @@
 
 #include "SDLApp.hpp"
 #include <kinematics/Puma560.hpp>
-#include <gui/Hello.hpp>
 #include <hwio/Hello.hpp>
 #include <iostream>
 
 
-int main(int argv, char** args) {
-
-    gui::Hello guiHello;
-    guiHello.hello();
+int main(int argv, char** args)
+{
     hwio::Hello hwioHello;
     hwioHello.hello();
 
